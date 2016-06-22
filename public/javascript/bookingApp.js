45327499cf3b1e9fcffd9ebaa6749ca0cdc3a3b1
Evan Bechtol <evanbechtol@gmail.com>
2016-06-22 09:50:56 -0500

    Added transitions and routing restrictions

---
+++ public/javascript/bookingApp.js
@@ -0,0 +1,10 @@
+var app = angular.module('bookingApp');
+
+app.run(function ($rootScope, $location, $route, authFactory) {
+  $rootScope.$on('$routeChangeStart', function (event, next, current) {
+    if (next.access.restricted && authFactory.isLoggedIn() === false) {
+      $location.path('/login');
+      $route.reload();
+    }
+  });
+});
