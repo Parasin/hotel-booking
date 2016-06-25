ebe1565107a6f00b189047c9aeb6161ca34dc052
Evan Bechtol <evanbechtol@gmail.com>
2016-06-25 00:17:39 -0500

    Testing connection logging

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
     res.sendFile('index.html', {root: __dirname});
-    console.log(req.connection.remoteAddress);
+    //console.log(req.connection.remoteAddress);
 });
 
 app.use(bodyParser.json());
