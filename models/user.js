2c650601f2f03c838e9961f9e3d56c77668b0606
Evan Bechtol <evanbechtol@gmail.com>
2016-07-01 14:19:55 -0500

    Updated POST /users/login

--- models/user.js
+++ models/user.js
@@ -71,7 +71,7 @@ module.exports = function (sequelize, DataTypes) {
         , "instanceMethods": {
             toPublicJSON: function () {
                 var json = this.toJSON();
-                return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt', 'firstName', 'lastName');
+                return _.pick(json, 'id', 'email', 'dateOfBirth', 'firstName', 'lastName');
             }
             , "generateToken": function (type) {
                 if (_.isString(type)) {
