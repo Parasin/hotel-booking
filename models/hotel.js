cb700899be2b8c5405201886529e7191f44c15f1
Evan Bechtol <evanbechtol@gmail.com>
2016-07-06 19:20:40 -0500

    Added hotel.js model

---
+++ models/hotel.js
@@ -0,0 +1,50 @@
+module.exports = function (sequelize, DataTypes) {
+    return sequelize.define('hotel', {
+        "phoneNo": {
+            "type": DataTypes.INTEGER
+            , "allowNull": false
+            , "validate": {
+                "isNumeric": true
+                , "len": [10, 12]
+            }
+        }
+        , "name": {
+            "type": DataTypes.STRING
+            , "allowNull": false
+            , "validate": {
+                is: ["^[a-z ]+$",'i']
+                , "notEmpty": true
+            }
+        }
+        , "street": {
+            "type": DataTypes.STRING
+            , "allowNull": false
+            , "validate": {
+                is: ["^[a-z0-9 ]+$",'i']
+                , "notEmpty": true
+            }
+        }
+        , "city": {
+            "type": DataTypes.STRING
+            , "allowNull": false
+            , "validate": {
+                "isAlpha": true
+            }
+        }
+        , "zipcode": {
+            "type": DataTypes.INTEGER
+            , "allowNull": false
+            , "validate": {
+                "isNumeric": true
+                , "len": [5]
+            }
+        }
+        , "country": {
+            "type": DataTypes.STRING
+            , "allowNull": false
+            , "validate": {
+                "isAlpha": true
+            }
+        }
+    });
+};
